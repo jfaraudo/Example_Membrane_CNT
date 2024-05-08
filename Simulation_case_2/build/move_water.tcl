3522aac36e754340b2a6353acc0c1b64e97ce3a3
Jordi Faraudo <jfaraudo@icmab.es>
2024-05-08 16:12:32 +0200

    Add files via upload

---
+++ Simulation_case_2/build/move_water.tcl
@@ -0,0 +1,57 @@
+set seleccio [atomselect top "same resid as (type OT and z>25"]
+set seleccio [atomselect top "same resid as (type OT and z>25)"]
+$seleccio num
+set numwater [atomselect top "same resid as (type OT and z>25)"]
+$numwater num
+set totwater [atomselect top "same resid as (type OT and z>25)"]
+$totwater num
+set totwater [atomselect top "water"]
+$totwater num
+set totwater [atomselect top "water and same resid as (type OT and z>27)"]
+set seleccio [atomselect top "water and same resid as (type OT and z>25)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>29)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>28)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.5)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.75)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.25)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.27)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.26)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.256)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.245)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.24)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.235)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.23)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.2)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.15)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.14)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.16)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.17)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.2)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.3)"]
+$seleccio num
+set seleccio [atomselect top "water and same resid as (type OT and z>27.4)"]
+$seleccio num
+$seleccio moveby {0 0 -54}
